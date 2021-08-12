f9aa8015f4477bd7f61d8717407b54d0dc83f3c1
Bernard Frimpong <bernie.frimpong@outlook.com>
2021-08-12 12:26:49 +0200

    Update Eli's age to 34

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ for (let outer = 0; outer < 2; outer += 1) {
           console.log(`${outer}-${inner}`);
         }for (let outer = 0; outer < 2; outer += 1) {
           for (let inner = 0; inner < 3; inner += 1) {
-            console.log(`${outer}-${inner}`);
+            console.log(`${outer}-${}`);
           }
