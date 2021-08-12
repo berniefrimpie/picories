b71670159ebb38bdca78653f1cae0789588c1c9a
Bernard Frimpong <bernie.frimpong@outlook.com>
2021-08-12 12:15:43 +0200

    Add Eli

---
+++ index.js
@@ -0,0 +1,20 @@
+for (let outer = 0; outer < 2; outer += 1) {
+  for (let inner = 0; inner < 3; inner += 1) {
+    console.log(`${outer}-${inner}`);
+  }
+}for (let outer = 0; outer < 2; outer += 1) {
+  for (let inner = 0; inner < 3; inner += 1) {
+    console.log(`${outer}-${inner}`);
+  }for (let outer = 0; outer < 2; outer += 1) {
+    for (let inner = 0; inner < 3; inner += 1) {
+      console.log(`${outer}-${inner}`);
+    }for (let outer = 0; outer < 2; outer += 1) {
+      for (let inner = 0; inner < 3; inner += 1) {
+        console.log(`${outer}-${inner}`);
+      }for (let outer = 0; outer < 2; outer += 1) {
+        for (let inner = 0; inner < 3; inner += 1) {
+          console.log(`${outer}-${inner}`);
+        }for (let outer = 0; outer < 2; outer += 1) {
+          for (let inner = 0; inner < 3; inner += 1) {
+            console.log(`${outer}-${inner}`);
+          }
